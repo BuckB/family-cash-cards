9d277eac34cc823959202c2fd39edf5feed6a0ad
Bruno Buck <b.brigagao@gmail.com>
2025-09-23 01:29:20 -0300

    Further json testing for correctness of fields

--- src/test/java/com/buckb/spring/academy/cashcard/CashCardJsonTest.java
+++ src/test/java/com/buckb/spring/academy/cashcard/CashCardJsonTest.java
@@ -1,5 +1,6 @@
 package com.buckb.spring.academy.cashcard;
 
+import java.io.IOException;
 import java.math.BigDecimal;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -34,4 +35,22 @@ public class CashCardJsonTest {
         assertThat(this.json.write(this.expectedEntity)).isStrictlyEqualToJson("expected.json");
     }
 
+    @Test
+    void cashCardSerialization_shouldContainIdAndAmount() throws Exception {
+        assertThat(this.json.write(this.expectedEntity)).hasJsonPathNumberValue("@.id");
+        assertThat(this.json.write(this.expectedEntity)).extractingJsonPathNumberValue("@.id").isEqualTo(99);
+        assertThat(this.json.write(this.expectedEntity)).hasJsonPathNumberValue("@.amount");
+        assertThat(this.json.write(this.expectedEntity)).extractingJsonPathNumberValue("@.amount").isEqualTo(123.45);
+    }
+
+    @Test
+    void cashCardDeserializationTest() throws IOException {
+        assertThat(this.json.parse(this.expectedJson)).isEqualTo(this.expectedEntity);
+    }
+
+    @Test
+    void cashCardDeserialization_shouldMatchIdAndAmount() throws IOException {
+        assertThat(this.json.parse(this.expectedJson)).hasFieldOrPropertyWithValue("id", 99L);
+        assertThat(this.json.parse(this.expectedJson)).hasFieldOrPropertyWithValue("amount", new BigDecimal("123.45"));
+    }
 }
