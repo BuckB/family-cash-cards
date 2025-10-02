caa49d031e9a67e1789dc0395461b160ad5a7712
Bruno Buck <b.brigagao@gmail.com>
2025-10-01 21:00:55 -0300

    RestController Test - should return 404 NOT_FOUND passing test

--- src/main/java/com/buckb/spring/academy/cashcard/CashCardController.java
+++ src/main/java/com/buckb/spring/academy/cashcard/CashCardController.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class CashCardController {
 
     @GetMapping("/{requestedId}")
-    public ResponseEntity<CashCard> findById() {
+    public ResponseEntity<CashCard> findById(@PathVariable Long requestedId) {
+        if (requestedId != 99L) {
+            return ResponseEntity.notFound().build();
+        }
         CashCard cashCard = new CashCard(99L, new BigDecimal("123.45"));
         return ResponseEntity.ok(cashCard);
     }
