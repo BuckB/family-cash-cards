7f8c69b11f0c143207f1a42a753a10ca5a446381
Bruno Buck <b.brigagao@gmail.com>
2025-10-04 02:21:03 -0300

    Rest Controller Test - Refactoring

--- src/test/java/com/buckb/spring/academy/cashcard/CashCardControllerTest.java
+++ src/test/java/com/buckb/spring/academy/cashcard/CashCardControllerTest.java
@@ -9,6 +9,9 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -19,20 +22,22 @@ class CashCardControllerTest {
 
     @Test
     void shouldReturnCashCardWhenDataIsSaved() {
-        ResponseEntity<CashCard> response = this.restTemplate.getForEntity("/cashcards/99", CashCard.class);
+        ResponseEntity<String> response = this.restTemplate.getForEntity("/cashcards/99", String.class);
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(response.getBody()).isNotNull();
-        assertThat(response.getBody().id()).isEqualTo(99L);
 
-        BigDecimal expectedAmount = new BigDecimal("123.45");
-        assertThat(response.getBody().amount()).isEqualByComparingTo(expectedAmount);
+        DocumentContext documentContext = JsonPath.parse(response.getBody());
+        Number id = documentContext.read("$.id");
+        Double amount = documentContext.read("$.amount");
+
+        assertThat(id).isEqualTo(99);
+        assertThat(amount).isEqualTo(123.45);
     }
 
     @Test
     void shouldReturn404WhenDataIsNotFound() {
-        ResponseEntity<CashCard> response = this.restTemplate.getForEntity("/cashcards/1", CashCard.class);
+        ResponseEntity<String> response = this.restTemplate.getForEntity("/cashcards/1", String.class);
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
-        assertThat(response.getBody()).isNull();
+        assertThat(response.getBody()).isBlank();
     }
 
     @Test
