9a047a7bdc1d1937551ce7819d7698c896eac37f
Bruno Buck <b.brigagao@gmail.com>
2025-10-04 02:22:44 -0300

    Controller Post Test - writting minimal code to pass
    Refactoring GetMapping to be more readable.

--- src/main/java/com/buckb/spring/academy/cashcard/CashCardController.java
+++ src/main/java/com/buckb/spring/academy/cashcard/CashCardController.java
@@ -1,8 +1,12 @@
 package com.buckb.spring.academy.cashcard;
 
+import java.util.Optional;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,10 +20,15 @@ public class CashCardController {
         this.cashCardRepository = cashCardRepository;
     }
 
-    @GetMapping("/{requestedId}")
-    public ResponseEntity<CashCard> findById(@PathVariable Long requestedId) {
-        return this.cashCardRepository.findById(requestedId)
-                .map(ResponseEntity::ok)
-                .orElse(ResponseEntity.notFound().build());
+    @GetMapping("/{id}")
+    public ResponseEntity<CashCard> findById(@PathVariable Long id) {
+        Optional<CashCard> response = this.cashCardRepository.findById(id);
+        return response.isPresent() ? ResponseEntity.ok(response.get()) : ResponseEntity.notFound().build();
+        // alternative: return response.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> create(@RequestBody CashCard newCashCard) {
+        return ResponseEntity.status(201).build();
     }
 }
