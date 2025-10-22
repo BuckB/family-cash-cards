b6511f73168b91df4f1fcc175a33f63b156c27e4
Bruno Buck <b.brigagao@gmail.com>
2025-10-22 12:20:36 -0300

    Adding dependency to load env files

--- src/main/java/com/buckb/spring/academy/cashcard/CashCardApplication.java
+++ src/main/java/com/buckb/spring/academy/cashcard/CashCardApplication.java
@@ -1,5 +1,7 @@
 package com.buckb.spring.academy.cashcard;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +9,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CashCardApplication {
 
 	public static void main(String[] args) {
+		Dotenv.configure()
+				.ignoreIfMalformed()
+				.systemProperties()
+				.load();
+
 		SpringApplication.run(CashCardApplication.class, args);
 	}
 
