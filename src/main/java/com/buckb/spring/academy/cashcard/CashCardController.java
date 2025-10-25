5ca4b03b29db73aef20bd8ee99ad57be177ef66c
Bruno Buck <b.brigagao@gmail.com>
2025-10-25 15:03:41 -0300

    Controller Delete Test - write minimal code to pass

--- src/main/java/com/buckb/spring/academy/cashcard/CashCardController.java
+++ src/main/java/com/buckb/spring/academy/cashcard/CashCardController.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -78,6 +79,11 @@ public class CashCardController {
                 updatedCashCard.amount(),
                 principal.getName());
         this.cashCardRepository.save(cardToSave);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id, Principal principal) {
         return ResponseEntity.noContent().build();
     }
 }
